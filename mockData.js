91405d1ad2cb6a092f95a49c4046648dddd36a50
evii02 <141530916+evii02@users.noreply.github.com>
2024-12-15 22:45:55 -0400

    Add files via upload

---
+++ mockData.js
@@ -0,0 +1,145 @@
+export const books = [
+    {
+      title: "Cien años de soledad",
+      description: "Una obra maestra del realismo mágico que narra la historia de la familia Buendía en el pueblo ficticio de Macondo.",
+      url: "/file-1734027875649.jpg",
+      autor: { name: "Gabriel García Márquez", nationality: "Colombiano" },
+      avaliable: true,
+      release: "1967-06-05"
+    },
+    {
+      title: "Damian4",
+      description: "La novela “Damián” de Alex Mírez es un éxito en la plataforma de escritura y lectura Wattpad. La historia se desarrolla en un pueblo ficticio y gira en torno a un grupo de personas que nacieron el 9 de septiembre y tienen tendencias asesinas.",
+      url: "https://i.pinimg.com/736x/7c/88/b1/7c88b1a14cf3586a5e97a3da08678c0f.jpg",
+      autor: { name: "Alex Mirez", nationality: "Venezolana" },
+      avaliable: true,
+      release: "2022-16-05"
+    },
+    {
+      title: "El gran Gatsby",
+      description: "Un retrato de la alta sociedad estadounidense de los años 20, centrado en el misterioso y millonario Jay Gatsby.",
+      url: "/file-1734027801523.jpeg",
+      autor: { name: "F. Scott Fitzgerald", nationality: "Estadounidense" },
+      avaliable: false,
+      release: "1925-04-10"
+    },
+    {
+      title: "Heist",
+      description: "En el juego de Heist, la unica salida es la muerte.",
+      url: "https://i.pinimg.com/736x/be/c9/e2/bec9e22f93b2b02725ec696ea5f1a3b9.jpg",
+      autor: { name: "Ariana Godoy", nationality: "Venezolana" },
+      avaliable: true,
+      release: "2021-06-05"
+    },
+    {
+      title: "El Hobbit",
+      description: "La aventura de Bilbo Bolsón, un hobbit que se embarca en una peligrosa misión con un grupo de enanos para recuperar su hogar.",
+      url: "/file-1734027734010.webp",
+      autor: { name: "J.R.R. Tolkien", nationality: "Británico" },
+      avaliable: true,
+      release: "1937-09-21"
+    },
+    {
+      title: "Orgullo y prejuicio",
+      description: "Una historia de amor y malentendidos entre Elizabeth Bennet y el orgulloso Sr. Darcy, ambientada en la sociedad inglesa del siglo XIX.",
+      url: "/file-1734027709680.jpg",
+      autor: { name: "Jane Austen", nationality: "Británica" },
+      avaliable: true,
+      release: "1813-01-28"
+    },
+    {
+      title: "Harry Potter y el pricionero de Azkaban",
+      description: "El prisionero de Azkaban, tercera aventura de Harry Potter, es uno de los mejores libros de J.K. Rowling. Fue publicado en 1999 y adaptado al cine en 2004. Si ya fue grande el éxito de La piedra filosofal y La cámara secreta, en este tercer libro la autora confirma  con creces su talento y el gran potencial de la saga.",     url: "/file-1734027674248.webp",
+      autor: { name: "J.K Rowling", nationality: "Estadounidense" },
+      avaliable: false,
+      release: "1999"
+    },
+    {
+      title: "La D no es por Dexter",
+      description: "Dexter Jefferson tiene la palabra desastre adherida a su nombre y últimamente eso parece estar más que científicamente comprobado. La prueba de ello puede ser el corazón roto que el mismo se encargó de obtener.",
+      url: "https://i.pinimg.com/736x/a5/ed/d5/a5edd580a57768748e57bb77b95d73ea.jpg",
+      autor: { name: "Darlis Steffany,", nationality: "Venezolana" },
+      avaliable: true,
+      release: "13-08-2023"
+    },
+    {
+      title: "Un cuento perfecto",
+      description: "¿Que sucede cuando descubres que el final de tu cuento no es como soñabas?",
+      url: "//i.pinimg.com/736x/79/b0/83/79b0831b7359584e2711fc79f4bc403c.jpg",
+      autor: { name: "Elisabet Benavetn", nationality: "Estadounidense" },
+      avaliable: true,
+      release: "02-02-2020"
+    },
+    {
+      title: "El código Da Vinci",
+      description: "Un thriller de misterio que mezcla arte, historia y religiones en una búsqueda frenética para resolver un antiguo enigma.",
+      url: "/file-1734027540226.jpeg",
+      autor: { name: "Dan Brown", nationality: "Estadounidense" },
+      avaliable: false,
+      release: "2003-03-18"
+    }
+  ];
+  
+
+  export const authors = [
+    {
+        name: "Gabriel García Márquez",
+        nationality: "Colombiano",
+        img: '/file-1734044242911.webp',
+        biography: "Gabriel García Márquez fue un escritor, periodista y novelista colombiano, conocido por ser uno de los máximos exponentes del realismo mágico. Ganó el Premio Nobel de Literatura en 1982, y su obra más famosa, 'Cien años de soledad', es un hito de la literatura universal."
+    },
+    {
+        name: "Darlys Steffany",
+        nationality: "Colombiana",
+        img: 'https://www.elinformadorve.com/wp-content/uploads/2022/11/WhatsApp-Image-2022-11-21-at-4.23.58-PM.jpeg',
+        biography: "George Orwell fue un novelista, ensayista y periodista británico, conocido por sus obras distópicas '1984' y 'Rebelión en la granja'. Sus escritos reflejan su oposición al totalitarismo y su compromiso con la justicia social."
+    },
+    {
+        name: "Arianna Godoy",
+        nationality: "Venezolana",
+        img: '//d24jx5gocr6em0.cloudfront.net/wp-content/uploads/2021/04/16173224/ariana-godoy-escritora.jpg',
+        biography: "Ariana Godoy es una Docente y autora venezolana, reside desde 2016 en Raleigh, Carolina del Norte. Comenzó su carrera como escritora en 2009 durante su etapa universitaria, alcanzando gran popularidad gracias a las publicaciones que hacía en la plataforma gratuita de lectura y escritura en línea, Wattpad."
+    },
+    {
+        name: "Harper Lee",
+        nationality: "Estadounidense",
+        img: '/file-1734044419233.webp',
+        biography: "Harper Lee fue una novelista estadounidense, conocida por su obra 'Matar a un ruiseñor', ganadora del Premio Pulitzer. La novela aborda temas como la injusticia racial y la pérdida de la inocencia en el sur de Estados Unidos."
+    },
+    {
+        name: "J.R.R. Tolkien",
+        nationality: "Británico",
+        img: '/file-1734044487708.webp',
+        biography: "J.R.R. Tolkien fue un escritor, filólogo y profesor británico, conocido por sus novelas de fantasía épica 'El Señor de los Anillos' y 'El Hobbit'. Es considerado el padre de la literatura fantástica moderna."
+    },
+    {
+        name: "Jane Austen",
+        nationality: "Británica",
+        img: '/file-1734044543264.avif',
+        biography: "Jane Austen fue una novelista británica conocida por sus agudas observaciones sobre la sociedad y las relaciones humanas. Entre sus obras más famosas se encuentran 'Orgullo y prejuicio' y 'Emma'."
+    },
+    {
+        name: "Miguel de Cervantes",
+        nationality: "Español",
+        img: '/file-1734044606702.webp',
+        biography: "Miguel de Cervantes fue un novelista, dramaturgo y poeta español, considerado una de las figuras más importantes de la literatura universal. Su obra maestra, 'Don Quijote de la Mancha', es un clásico que marcó el inicio de la novela moderna."
+    },
+    {
+        name: "Victor Hugo",
+        nationality: "Francés",
+        img: '/file-1734044652109.jpg',
+        biography: "Victor Hugo fue un escritor, poeta y dramaturgo francés, considerado una de las figuras literarias más importantes del Romanticismo. Entre sus obras más célebres se encuentran 'Los Miserables' y 'Nuestra Señora de París'."
+    },
+    {
+        name: "Ray Bradbury",
+        nationality: "Estadounidense",
+        img: '/file-1734044733813.jpg',
+        biography: "Ray Bradbury fue un escritor estadounidense conocido por sus obras de ciencia ficción y fantasía. Su novela 'Fahrenheit 451' es una crítica al autoritarismo y la censura, y sigue siendo un clásico de la literatura distópica."
+    },
+    {
+        name: "Dan Brown",
+        nationality: "Estadounidense",
+        img: '/file-1734044782630.jpg',
+        biography: "Dan Brown es un novelista estadounidense conocido por sus thrillers de misterio que mezclan historia, arte y simbolismo. Su obra más famosa, 'El código Da Vinci', ha sido un éxito internacional y ha inspirado películas y debates académicos."
+    }
+];
